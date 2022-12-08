6390a61cbf0412f420cd6f42e8fe745e6fcb9d88
Ben <bafelson@gmail.com>
2022-12-08 00:41:33 -0500

    Lesson 1 -- basic window and surface

--- game430.py
+++ game430.py
@@ -1,2 +1,53 @@
-import pygame as pg
-print("Testing!")
+'''
+ORDER FOR TEACHING/CODING
+1. Import and init
+2. Create display surface
+3. main event loop
+4. quit and update
+5. clock
+6. Non-display surfaces 
+
+'''
+
+import pygame
+from sys import exit #using to quit the program effectively
+
+pygame.init() #necessary to run before any other code, initiates the library so to speak
+
+#create display surface
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))   #creates an actual window of size width, height
+#after above code, window should open briefly, but just for a second. we need to make it run forever
+
+#Create a title
+pygame.display.set_caption("Runner Game")
+
+#Clock Creation
+clock = pygame.time.Clock() #clock object
+
+#Non-display surfaces
+test_width = 100
+test_height = 200
+test_surface = pygame.Surface((test_width, test_height))    #creates surface with given height and width
+test_surface.fill('cyan')   #need to change color so it displays on black background
+
+
+
+#main event loop
+while True:
+    #check player inputs
+    for event in pygame.event.get():
+        #quit
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit() #need to get out of the loop, otherwise will throw an error
+
+
+    #draw all elements
+    screen.blit(test_surface, (0,0))    #displays surfaces on screen at given position
+
+    #update everything
+
+    pygame.display.update() #updates display surface (screen)
+    clock.tick(60)  #runs at 60fps MAXIMUM (explain framerates)
