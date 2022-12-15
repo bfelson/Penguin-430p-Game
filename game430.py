549a413a7f00f849c448d2c67ff689f64f3d2acf
Ben <66187480+bfelson@users.noreply.github.com>
2022-12-14 19:36:11 -0500

    update class 1

--- game430.py
+++ game430.py
@@ -1,12 +1,11 @@
 '''
-ORDER FOR TEACHING/CODING
+Class 1: Ends 30:36
 1. Import and init
 2. Create display surface
 3. main event loop
 4. quit and update
 5. clock
 6. Non-display surfaces 
-
 '''
 
 import pygame
@@ -21,7 +20,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))   #creates an actual window of
 #after above code, window should open briefly, but just for a second. we need to make it run forever
 
 #Create a title
-pygame.display.set_caption("Runner Game")
+pygame.display.set_caption("Ben's 4:30PM Python Game")
 
 #Clock Creation
 clock = pygame.time.Clock() #clock object
@@ -29,7 +28,7 @@ clock = pygame.time.Clock() #clock object
 #Non-display surfaces
 test_width = 100
 test_height = 200
-test_surface = pygame.Surface((test_width, test_height))    #creates surface with given height and width
+test_surface = pygame.Surface((WIDTH/2 - test_width/2, HEIGHT/2 - test_height/2))    #creates surface with given height and width
 test_surface.fill('cyan')   #need to change color so it displays on black background
 
 
